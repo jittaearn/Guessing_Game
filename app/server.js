a7b41d21523c7f76c61b9c59cc41fdab3e375803
Jitta Koopratoomsiri <jitta.k@ku.th>
2020-04-13 22:34:48 +0700

    try game

--- app/server.js
+++ app/server.js
@@ -1,24 +1,16 @@
 'use strict'; 
 const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 // Connection URL
-const url = 'mongodb://mongodb:27017';
+const url = 'mongodb://localhost:27017';
 
 // Database Name
 const dbName = 'pantip';
 // Create a new MongoClient
 const client = new MongoClient(url, { useUnifiedTopology: true });
 
-const urlLogger = (request, response, next) => {
-  console.log('Request URL:', request.url);
-  next();
-};
-
-const timeLogger = (request, response, next) => {
-  console.log('Datetime:', new Date(Date.now()).toString());
-  next();
-};
 
 // Constants
 const PORT = 9000;
@@ -26,19 +18,41 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-
-app.use(urlLogger, timeLogger);
 app.use(express.static('public'));
-
-app.get('/', (request, response) => {
-  // We don't need to explicitly use this handler or send a response
-  // because Express is using the default path of the static assets
-  // to serve this content
-});
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/json', (request, response) => {
-  response.status(200).json({"name": "Robbie"});
+  response.status(200).json({"name": "Jitta"});
 });
 
-app.listen(PORT, () => {
-  console.log(`Running on http://${HOST}:${PORT}`)})
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Successfully connected to server");
+
+  const db = client.db(dbName);
+  const col = db.collection('game');
+
+  // get index html template
+  app.get('/', async (req, res) => {
+    // Get first two documents that match the query
+    col.find({}).limit(1).toArray(function(err, docs) {
+      assert.equal(null, err);
+      // res.send(JSON.stringify(docs));
+      res.render('index');
+      client.close();
+    });
+  });
+  //receive data when users hit the button
+  app.post('/', async function(req, res) {
+    console.log(req.body.name);
+    res.render('index');
+  })
+});
+
+
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
+
